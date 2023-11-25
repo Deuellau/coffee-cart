d27d99c0b3f223ad914342a8a7a74109e130d8a8
Deuel <deuellautc@gmail.com>
2023-11-25 23:51:44 +0800

    added menu_to_csv func

--- file_functions.py
+++ file_functions.py
@@ -1,3 +1,5 @@
+import csv
+
 class File_functions:
     def csv_to_menu(file_path, menu):
         try:
@@ -22,5 +24,17 @@ class File_functions:
             print(f'Successfully added item(s) from {file_path} file.\n\n')
         except FileNotFoundError:
             print(f'File not found: {file_path}')
+        except Exception as e:
+            print(f'An error occurred: {e}')
+            
+    def menu_to_csv(file_path, menu):
+        try:
+            with open(file_path, 'w', newline='') as file:
+                csv_writer = csv.writer(file)
+                for item in menu.items:
+                    csv_writer.writerow([item.name, item.price])
+            print('Menu items have been successfully written to menu.csv.\n\n')
+        except FileNotFoundError:
+            print('Error: The specified file was not found.')
         except Exception as e:
             print(f'An error occurred: {e}')
